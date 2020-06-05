f840469b2e3badf48a2ce51798e81d49186735c1
Alexander Tsoy <alexander@tsoy.me>
2020-06-05 17:21:10 +0300

    Do not strip lv2 plugins

--- trunk/waftools/lv2.py
+++ trunk/waftools/lv2.py
@@ -40,7 +40,7 @@ def lv2_add_common(tg, target, install_path, defines=None, linkflags=None, cxxfl
 @Configure.conf
 def lv2(bld, *k, **kw):
     lv2_base, dst = get_lv2_base(bld, kw)
-    tg = bld.shlib(features='strip', *k, **kw)
+    tg = bld.shlib(*k, **kw)
     cxxflags = []
     if not bld.env['OPT'] and bld.env['SSE2']:
         cxxflags = [ "-msse2", "-mfpmath=sse"]
@@ -57,7 +57,7 @@ def lv2_gui(bld, *k, **kw):
     if not bld.env['LV2GUI']:
         return None
     lv2_base, dst = get_lv2_base(bld, kw)
-    tg = bld.shlib(features='strip', *k, **kw)
+    tg = bld.shlib(*k, **kw)
     lv2_add_common(tg, lv2_base+'_gui', dst, ["LV2_GUI"], bld.env['OS_LV2_CXXFLAGS'])
     return tg
 
